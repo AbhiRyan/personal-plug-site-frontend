5ce67b381bc401973cbbdf8f27605cac9c16f8fa
AbhiRyan <abhi.ryan@yahoo.com>
2024-03-11 15:45:30 +1030

    api adress update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'pps-spring-backend.azurewebsites.net/api',
+  API_URL: 'pps-spring-backend.azurewebsites.net:8080/api',
   // API_URL: 'https://host.docker.internal:8080/api',
 };
